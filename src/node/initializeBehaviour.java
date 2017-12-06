ce0fa66efb6f1263b8bb6e6a6be7bbe1875e4299
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-12-05 22:55:13 -0200

    [Refactor] Reducing code repetition in initializeBehaviour

--- src/node/initializeBehaviour.java
+++ src/node/initializeBehaviour.java
@@ -10,15 +10,19 @@ import jade.core.behaviours.WakerBehaviour;
 import models.NodeAgentData;
 
 public class initializeBehaviour extends WakerBehaviour {
-	
 	private static final long serialVersionUID = -2905010008367981124L;
+
+	// Constants for initializeChildrenValues method
+	private static final int LOWER_BOUND = 0;
+	private static final int UPPER_BOUND = 1;
+	private static final int THRESHOLD = 2;
 
 	NodeAgentData data = new NodeAgentData();
 	public initializeBehaviour(Agent a, long period, NodeAgentData data) {
 		super(a, period);
 		this.data = data;
 	}
-	
+
 	@Override
 	public void onWake() {
 		System.out.println("[INITIALIZE ]" +myAgent.getLocalName()+" starting initialize procedure");
@@ -27,47 +31,9 @@ public class initializeBehaviour extends WakerBehaviour {
         data.setThreshold(0);
         data.setCurrentContext(new HashMap<>());		
         
-        // Initialize children lower bounds
-        List<List<Integer>> childrenLowerBounds = new ArrayList<>();
-        for (int i = 0; i < data.getDomain().size(); i++) {
-            List<Integer> childrenLowerBoundsForDomain = new ArrayList<>();
-            
-            for(int j = 0; j < data.getChildren().size(); j++) {
-            	childrenLowerBoundsForDomain.add(0);
-            	System.out.println("[LOWER BOUND] lb(" + i + "," + j + ") = " + childrenLowerBoundsForDomain.get(j));
-            }
-            
-            childrenLowerBounds.add(childrenLowerBoundsForDomain);
-        }
-        data.setChildrenLowerBounds(childrenLowerBounds);
-        
-        // Initialize children upper bounds
-        List<List<Integer>> childrenUpperBounds = new ArrayList<>();
-        for (int i = 0; i < data.getDomain().size(); i++) {
-            List<Integer> childrenUpperBoundsForDomain = new ArrayList<>();
-            
-            for(int j = 0; j < data.getChildren().size(); j++) {
-            	childrenUpperBoundsForDomain.add(Integer.MAX_VALUE);
-            	System.out.println("[UPPER BOUND] ub(" + i + "," + j + ") = " + childrenUpperBoundsForDomain.get(j));
-            }
-            
-            childrenLowerBounds.add(childrenUpperBoundsForDomain);
-        }
-        data.setChildrenUpperBounds(childrenUpperBounds);
-        
-        // Initialize children thresholds
-        List<List<Integer>> childrenThresholds = new ArrayList<>();
-        for (int i = 0; i < data.getDomain().size(); i++) {
-            List<Integer> childrenThresholdsForDomain = new ArrayList<>();
-            
-            for(int j = 0; j < data.getChildren().size(); j++) {
-            	childrenThresholdsForDomain.add(0);
-            	System.out.println("[TRHESHOLD  ] t(" + i + "," + j + ") = " + childrenThresholdsForDomain.get(j));
-            }
-            
-            childrenThresholds.add(childrenThresholdsForDomain);
-        }
-        data.setChildrenThresholds(childrenThresholds);
+        data.setChildrenLowerBounds(initializeChildrenValues(LOWER_BOUND));
+        data.setChildrenUpperBounds(initializeChildrenValues(UPPER_BOUND));
+        data.setChildrenThresholds(initializeChildrenValues(THRESHOLD));
         
         // Initialize children contexts
         List<List<Map<String, Integer>>> childrenContexts = new ArrayList<>();
@@ -82,11 +48,42 @@ public class initializeBehaviour extends WakerBehaviour {
             childrenContexts.add(childrenContextsForDomain);
         }
         data.setChildrenContexts(childrenContexts);
-        
-        
+
 		data.setChosenValue(data.getDomain().get(0)); // to do: di <- d that minimizes LB(d) 
 		
 		myAgent.addBehaviour(new receiveValueMessageBehaviour(myAgent));
 		myAgent.addBehaviour(new backTrackBehaviour(myAgent, data));
 	}
+
+    private List<List<Integer>> initializeChildrenValues(int valueType) {
+        List<List<Integer>> values = new ArrayList<>();
+
+        for (int i = 0; i < data.getDomain().size(); i++) {
+            List<Integer> valuesForDomain = new ArrayList<>();
+
+            for (int j = 0; j < data.getChildren().size(); j++) {
+                switch (valueType) {
+                    case LOWER_BOUND: {
+                        valuesForDomain.add(0);
+                        System.out.println("[LOWER BOUND] lb(" + i + "," + j + ") = " + valuesForDomain.get(j));
+                        break;
+                    }
+                    case UPPER_BOUND: {
+                        valuesForDomain.add(Integer.MAX_VALUE);
+                        System.out.println("[UPPER BOUND] ub(" + i + "," + j + ") = " + valuesForDomain.get(j));
+                        break;
+                    }
+                    case THRESHOLD: {
+                        valuesForDomain.add(0);
+                        System.out.println("[THRESHOLD  ] t(" + i + "," + j + ") = " + valuesForDomain.get(j));
+                        break;
+                    }
+                }
+            }
+
+            values.add(valuesForDomain);
+        }
+
+        return values;
+    }
 }
