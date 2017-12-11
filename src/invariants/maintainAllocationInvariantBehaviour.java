88ad56a319086a46c424c14eecb4feb956fe7b3d
Felipe Hargreaves <fhargreaves00@gmail.com>
2017-12-11 21:32:37 -0200

    Add maintain allocation invariant behaviour.

---
+++ src/invariants/maintainAllocationInvariantBehaviour.java
@@ -0,0 +1,77 @@
+package invariants;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import messages.ThresholdMessage;
+import models.NodeAgentData;
+import node.NodeAgent;
+import node.sendMessageBehaviour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class maintainAllocationInvariantBehaviour extends OneShotBehaviour {
+    NodeAgentData data;
+
+    public maintainAllocationInvariantBehaviour(Agent a, NodeAgentData data) {
+        super(a);
+        this.data = data;
+    }
+
+    @Override
+    public void action() {
+        System.out.println("[INV MAIB   ] " + myAgent.getLocalName() +
+                           " starting maintain allocation invariant");
+
+        int childrenThresholds;
+        int currentValue;
+        List<List<Integer>> childrenThresholdsList;
+        List<List<Integer>> childrenUpperBoundsList;
+        List<List<Integer>> childrenLowerBoundsList;
+        List<AID> childrenList;
+
+        childrenThresholds = 0;
+        currentValue = data.getChosenValue(); // unsure about this
+        childrenThresholdsList = data.getChildrenThresholds();
+        childrenUpperBoundsList = data.getChildrenUpperBounds();
+        childrenLowerBoundsList = data.getChildrenLowerBounds();
+        childrenList = data.getChildren();
+
+        for (int childThreshold : childrenThresholdsList.get(currentValue)) {
+            childrenThresholds += childThreshold;
+        }
+
+        while (data.getThreshold() > currentValue + childrenThresholds) {
+            for (int i = 0; i < childrenThresholdsList.get(currentValue).size(); i++) {
+                int childUpperBound = childrenUpperBoundsList.get(currentValue).get(i);
+                int childThreshold = childrenThresholdsList.get(currentValue).get(i);
+
+                if (childUpperBound > childThreshold) {
+                    data.setChildThreshold(currentValue, i, childThreshold++);
+                }
+            }
+        }
+
+        while (data.getThreshold() < currentValue + childrenThresholds) {
+            for (int i = 0; i < childrenThresholdsList.get(currentValue).size(); i++) {
+                int childLowerBound = childrenLowerBoundsList.get(currentValue).get(i);
+                int childThreshold = childrenThresholdsList.get(currentValue).get(i);
+
+                if (childLowerBound < childThreshold) {
+                    data.setChildThreshold(currentValue, i, childThreshold--);
+                }
+            }
+        }
+
+        for (int i = 0; i < childrenList.size(); i++) {
+            int valueToSend = childrenThresholdsList.get(currentValue).get(i);
+
+            List<AID> childToSend = new ArrayList<>();
+            childToSend.add(childrenList.get(i));
+
+            myAgent.addBehaviour(new sendMessageBehaviour(myAgent,
+                    data, new ThresholdMessage(valueToSend), childToSend));
+        }
+    }
+}
