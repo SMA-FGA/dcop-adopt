613d99b8dfe7cbd3d7558877cf2e2b95eef5eb90
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-02-20 13:56:16 -0300

    Adapt children bounds/threshold usage in maintainChildThresholdInvariant.

--- src/invariants/maintainChildThresholdInvariantBehaviour.java
+++ src/invariants/maintainChildThresholdInvariantBehaviour.java
@@ -4,6 +4,9 @@ import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 import models.NodeAgentData;
 
+import java.util.List;
+import java.util.Map;
+
 public class maintainChildThresholdInvariantBehaviour extends OneShotBehaviour{
 
 	private static final long serialVersionUID = -1491184944608291784L;
@@ -17,19 +20,30 @@ public class maintainChildThresholdInvariantBehaviour extends OneShotBehaviour{
 	@Override
 	public void action() {
 		System.out.println("[INV MCTI   ] "+myAgent.getLocalName()+" starting maintain child threshold invariant");
-		
-        for (int i = 0; i < data.getDomain().size(); i++) { 
-            for(int j = 0; j < data.getChildren().size(); j++) {
-            	while(data.getChildrenLowerBounds().get(i).get(j) > data.getChildrenThresholds().get(i).get(j)) {
-            		data.setChildThreshold(i, j, data.getChildrenThresholds().get(i).get(j) + 1);
-            	}
-            }
-            for(int j = 0; j < data.getChildren().size(); j++) {
-            	while(data.getChildrenLowerBounds().get(i).get(j) > data.getChildrenThresholds().get(i).get(j)) {
-            		data.setChildThreshold(i, j, data.getChildrenThresholds().get(i).get(j) - 1);
-            	} 
-            }
-        } 
+
+		for (Map.Entry<String, List<Integer>> child : data.getChildrenThresholds().entrySet()) {
+			for (int i = 0; i < data.getDomain().size(); i++) {
+				int lowerBound = data.getChildrenLowerBounds().get(child.getKey()).get(i);
+				int currentThreshold = child.getValue().get(i);
+
+				while (lowerBound > currentThreshold) {
+					currentThreshold += 1;
+					data.setChildThreshold(i, child.getKey(), currentThreshold);
+				}
+			}
+		}
+
+		for (Map.Entry<String, List<Integer>> child : data.getChildrenThresholds().entrySet()) {
+			for (int i = 0; i < data.getDomain().size(); i++) {
+				int upperBound = data.getChildrenUpperBounds().get(child.getKey()).get(i);
+				int currentThreshold = child.getValue().get(i);
+
+				while (currentThreshold > upperBound) {
+					currentThreshold -= 1;
+					data.setChildThreshold(i, child.getKey(), currentThreshold);
+				}
+			}
+		}
 	}
 	
 }
