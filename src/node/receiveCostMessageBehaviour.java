a6429c8a5d9633a36fe19c07c032d05bbc9884d7
Jeferson Alves <jefersonalvesf2009@gmail.com>
2018-03-01 17:19:57 -0300

    Add action when no terminate was received in the receive cost behaviour

--- src/node/receiveCostMessageBehaviour.java
+++ src/node/receiveCostMessageBehaviour.java
@@ -1,5 +1,8 @@
 package node;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import invariants.maintainChildThresholdInvariantBehaviour;
 import invariants.maintainThresholdInvariantBehaviour;
 import jade.core.Agent;
@@ -11,10 +14,9 @@ import messages.CostMessage;
 import messages.MessageTypes;
 import models.NodeAgentData;
 
-import java.util.Map;
-
 
 public class receiveCostMessageBehaviour extends CyclicBehaviour  implements MessageTypes{
+	
 	private static final long serialVersionUID = -6895391790742950856L;
 	NodeAgentData data;
 	
@@ -37,34 +39,69 @@ public class receiveCostMessageBehaviour extends CyclicBehaviour  implements Mes
 			}
 			
 			if(adoptMessage.getMessageType() == COST_MESSAGE) {
-				CostMessage cost = null;
 				try {
-					cost = (CostMessage) message.getContentObject();
+					CostMessage cost = (CostMessage) message.getContentObject();
+					
+					System.out.println("[REC COST   ] "+myAgent.getLocalName()+
+							   " receive cost message: " + cost.toString()+
+							   " from "+message.getSender().getLocalName());
 
-//					Map<String, Integer> contextInMessage = cost.getContext();
-//					int valueInMessage = contextInMessage.get(myAgent.getLocalName());
-//					data.setCurrentValue(valueInMessage);
-//
-//					contextInMessage.remove(myAgent.getLocalName());
-//
-//					if (!data.hasReceivedTerminate()) {
-//						// TODO
-//					}
-//
-//					if (data.isContextCompatible(contextInMessage)) {
-//						int currentValue = data.getCurrentValue();
-//						String xk = cost.getXk();
-//
-//						// TODO: update bounds and context
-//
-//						myAgent.addBehaviour(new maintainChildThresholdInvariantBehaviour(myAgent, data));
-//						myAgent.addBehaviour(new maintainThresholdInvariantBehaviour(myAgent, data));
-//					}
+					Map<String, Integer> contextInMessage = cost.getContext();
+					Integer value = data.getCurrentValue();
+					
+					if(contextInMessage.containsKey(myAgent.getLocalName())) {
+						value = contextInMessage.get(myAgent.getLocalName());
+						data.setCurrentValue(value);
+					}
 
+					contextInMessage.remove(myAgent.getLocalName());
+
+					if(!data.hasReceivedTerminate()) {
+						Map<String, Integer> contextWithNotNeighbours = data.getCurrentContext();
+						
+						for (Map.Entry<String, Integer> pair : contextInMessage.entrySet()) {
+							if(!data.isMyNeighbour(pair.getKey())) {
+								contextWithNotNeighbours.put(pair.getKey(), pair.getValue());
+							}else {
+								//do nothing
+							}
+				        }
+						data.setCurrentContext(contextWithNotNeighbours);
+						
+						for (int d = 0; d < data.getDomain().size(); d++) {
+				    		for(int c = 0; c < data.getChildren().size(); c++) {
+				    			if(!data.isContextCompatible(data.getChildrenContexts().get(d).get(c))) {
+				    				String child = data.getChildren().get(c).getLocalName();
+
+				    				data.setChildLowerBound(d, child, 0);
+					    			data.setChildThreshold(d, child, 0);
+					    			data.setChildUpperBound(d, child, Integer.MAX_VALUE);
+					    			data.setChildContext(d, c, new HashMap<String, Integer>());
+				    			}else {
+				    				//do nothing
+				    			}
+				            }
+				    	}
+					}else {
+						//do nothing
+					}
+					
+					if(data.isContextCompatible(contextInMessage)) {
+						String sender = cost.getSender();
+						
+						//data.setChildLowerBound(value, sender, cost.getLowerBound());
+						//data.setChildUpperBound(value,  sender, cost.getUpperBound());
+						//data.setChildContext(value,  sender, cost.getContext());
+						
+						myAgent.addBehaviour(new maintainChildThresholdInvariantBehaviour(myAgent, data));
+						myAgent.addBehaviour(new maintainThresholdInvariantBehaviour(myAgent, data));
+					}
+					myAgent.addBehaviour(new backTrackBehaviour(myAgent, data));
+					
 				} catch (UnreadableException e) {
 					e.printStackTrace();
 				}
-				System.out.println("[REC COST   ] "+myAgent.getLocalName()+" receive cost message: " + cost.toString()+" from "+message.getSender().getLocalName());
+				
 			}
 			
 		}else {
